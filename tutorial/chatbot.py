56926d61c85820f02e36c1b08220bac5465b2226
Roger <roger.issonaga@gmail.com>
2024-11-02 16:15:26 -0300

    langraph and states

---
+++ tutorial/chatbot.py
@@ -0,0 +1,73 @@
+#%%
+import os
+import getpass
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+
+#%%
+load_dotenv(dotenv_path=".env", override=True)
+
+#%%
+model = ChatOpenAI(model="gpt-4o-mini")
+# %%
+model.invoke(
+    [
+        HumanMessage(content="Hi! I'm Bob"),
+        AIMessage(content="Hello Bob! How can I assist you today?"),
+        HumanMessage(content="What's my name?"),
+    ]
+)
+# %%
+
+workflow = StateGraph(state_schema=MessagesState)
+# %%
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
+
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+# %%
+config = {"configurable": {"thread_id": "abc123"}}
+# %%
+query = "Hi! I'm Bob."
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()  # output contains all messages in state
+# %%
+query = "What's my name?"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+# %%
+input_messages
+# %%
+config = {"configurable": {"thread_id": "abc234"}}
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+# %%
+config = {"configurable": {"thread_id": "abc123"}}
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+# %%
+
+# %%
