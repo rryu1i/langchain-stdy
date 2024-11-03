d0345f8ed0f93f8380255d3a6b45a9bfae4d8a0c
Roger <roger.issonaga@gmail.com>
2024-11-02 21:33:15 -0300

    added chat history

--- tutorial/chatbot.py
+++ tutorial/chatbot.py
@@ -8,66 +8,68 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
-
+from typing import Sequence
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
 #%%
 load_dotenv(dotenv_path=".env", override=True)
 
 #%%
 model = ChatOpenAI(model="gpt-4o-mini")
+parser = StrOutputParser()
+
 # %%
-model.invoke(
+prompt = ChatPromptTemplate.from_messages(
     [
-        HumanMessage(content="Hi! I'm Bob"),
-        AIMessage(content="Hello Bob! How can I assist you today?"),
-        HumanMessage(content="What's my name?"),
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
     ]
 )
-# %%
 
-workflow = StateGraph(state_schema=MessagesState)
-# %%
-
-# Define the function that calls the model
-def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
-    return {"messages": response}
+#%%
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
 
 
-# Define the (single) node in the graph
+workflow = StateGraph(state_schema=State)
+
+
+def call_model(state: State):
+    chain = prompt | model
+    response = chain.invoke(state)
+    return {"messages": [response]}
+
+
 workflow.add_edge(START, "model")
 workflow.add_node("model", call_model)
 
-# Add memory
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
-# %%
-config = {"configurable": {"thread_id": "abc123"}}
-# %%
+
+#%%
+
+config = {"configurable": {"thread_id": "abc456"}}
 query = "Hi! I'm Bob."
+language = "Spanish"
 
 input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
-output["messages"][-1].pretty_print()  # output contains all messages in state
-# %%
-query = "What's my name?"
-
-input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
+output = app.invoke(
+    {"messages": input_messages, "language": language},
+    config,
+)
 output["messages"][-1].pretty_print()
 # %%
-input_messages
-# %%
-config = {"configurable": {"thread_id": "abc234"}}
+query = "What is my name?"
 
 input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
+output = app.invoke(
+    {"messages": input_messages},
+    config,
+)
 output["messages"][-1].pretty_print()
-# %%
-config = {"configurable": {"thread_id": "abc123"}}
-
-input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
-output["messages"][-1].pretty_print()
-# %%
-
 # %%
